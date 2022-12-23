9bd4a1daad68d4071660b46a0d6356f4953aca50
brentellingson <47702944+brentellingson@users.noreply.github.com>
2022-12-22 21:17:18 -0600

    chap 8a: statements (#5)

--- lox/test/com/craftinginterpreters/lox/ParserTest.java
+++ lox/test/com/craftinginterpreters/lox/ParserTest.java
@@ -7,77 +7,77 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
-    private static String parse(String source) {
+    private static String expression(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        Expr expression = parser.expression();
 
          return new AstPrinter().print(expression);
     }
 
     @Test
-    void parseEquality() {
+    void expressionEquality() {
         assertAll(
-                () -> assertEquals("(!= (== 1.0 2.0) 3.0)", parse("1 == 2 != 3"))
+                () -> assertEquals("(!= (== 1.0 2.0) 3.0)", expression("1 == 2 != 3"))
         );
     }
 
     @Test
-    void parseComparison() {
+    void expressionComparison() {
         assertAll(
-                () -> assertEquals("(>= (> (<= (< 1.0 2.0) 3.0) 4.0) 5.0)", parse("1 < 2 <= 3 > 4 >= 5"))
+                () -> assertEquals("(>= (> (<= (< 1.0 2.0) 3.0) 4.0) 5.0)", expression("1 < 2 <= 3 > 4 >= 5"))
         );
     }
 
     @Test
-    void parseTerm() {
+    void expressionTerm() {
         assertAll(
-                () -> assertEquals("(- (+ 1.0 2.0) 3.0)", parse("1 + 2 - 3"))
+                () -> assertEquals("(- (+ 1.0 2.0) 3.0)", expression("1 + 2 - 3"))
         );
     }
 
     @Test
-    void parseFactor() {
+    void expressionFactor() {
         assertAll(
-                () -> assertEquals("(/ (* 1.0 2.0) 3.0)", parse("1 * 2 / 3"))
+                () -> assertEquals("(/ (* 1.0 2.0) 3.0)", expression("1 * 2 / 3"))
         );
     }
 
     @Test
-    void parseUnary() {
+    void expressionUnary() {
         assertAll(
-                () -> assertEquals("(! (- 1.0))", parse("! - 1"))
+                () -> assertEquals("(! (- 1.0))", expression("! - 1"))
         );
     }
 
     @Test
-    void parsePrimary() {
+    void expressionPrimary() {
         assertAll(
-                () -> assertEquals("true", parse("true")),
-                () -> assertEquals("false", parse("false")),
-                () -> assertEquals("nil", parse("nil")),
-                () -> assertEquals("1.0", parse("1")),
-                () -> assertEquals("hello", parse("\"hello\""))
+                () -> assertEquals("true", expression("true")),
+                () -> assertEquals("false", expression("false")),
+                () -> assertEquals("nil", expression("nil")),
+                () -> assertEquals("1.0", expression("1")),
+                () -> assertEquals("hello", expression("\"hello\""))
         );
     }
 
     @Test
-    void parsePrecedence() {
+    void expressionPrecedence() {
         assertAll(
-                () -> assertEquals("(== 1.0 (< 2.0 3.0))", parse("1 == 2 < 3")),
-                () -> assertEquals("(== (< 1.0 2.0) 3.0)", parse("1 < 2 == 3")),
-                () -> assertEquals("(> 1.0 (+ 2.0 3.0))", parse("1 > 2 + 3")),
-                () -> assertEquals("(> (+ 1.0 2.0) 3.0)", parse("1 + 2 > 3")),
-                () -> assertEquals("(+ 1.0 (* 2.0 3.0))", parse("1 + 2 * 3")),
-                () -> assertEquals("(+ (* 1.0 2.0) 3.0)", parse("1 * 2 + 3")),
-                () -> assertEquals("(* (- 1.0) 2.0)", parse("- 1 * 2")),
-                () -> assertEquals("(* 1.0 (- 2.0))", parse("1 * - 2")),
-                () -> assertEquals("(- (group 1.0))", parse("-(1)")),
-                () -> assertEquals("(group (- 1.0))", parse("(-1)")),
-                () -> assertEquals("(== 1.0 (< 2.0 (+ 3.0 (* 4.0 (- (group (+ 5.0 6.0)))))))", parse("1 == 2 < 3 + 4 * - (5 + 6)")),
-                () -> assertEquals("(== (< (+ (* (- (group (+ 1.0 2.0))) 3.0) 4.0) 5.0) 6.0)", parse("-(1 + 2) * 3 + 4 < 5 == 6"))
+                () -> assertEquals("(== 1.0 (< 2.0 3.0))", expression("1 == 2 < 3")),
+                () -> assertEquals("(== (< 1.0 2.0) 3.0)", expression("1 < 2 == 3")),
+                () -> assertEquals("(> 1.0 (+ 2.0 3.0))", expression("1 > 2 + 3")),
+                () -> assertEquals("(> (+ 1.0 2.0) 3.0)", expression("1 + 2 > 3")),
+                () -> assertEquals("(+ 1.0 (* 2.0 3.0))", expression("1 + 2 * 3")),
+                () -> assertEquals("(+ (* 1.0 2.0) 3.0)", expression("1 * 2 + 3")),
+                () -> assertEquals("(* (- 1.0) 2.0)", expression("- 1 * 2")),
+                () -> assertEquals("(* 1.0 (- 2.0))", expression("1 * - 2")),
+                () -> assertEquals("(- (group 1.0))", expression("-(1)")),
+                () -> assertEquals("(group (- 1.0))", expression("(-1)")),
+                () -> assertEquals("(== 1.0 (< 2.0 (+ 3.0 (* 4.0 (- (group (+ 5.0 6.0)))))))", expression("1 == 2 < 3 + 4 * - (5 + 6)")),
+                () -> assertEquals("(== (< (+ (* (- (group (+ 1.0 2.0))) 3.0) 4.0) 5.0) 6.0)", expression("-(1 + 2) * 3 + 4 < 5 == 6"))
         );
     }
 }
